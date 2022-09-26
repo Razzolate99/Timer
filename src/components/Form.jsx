7656b09de7bb75207efeb0858d2cffefd897de58
Aaron <46139215+Razzolate99@users.noreply.github.com>
2022-09-26 13:52:34 -0400

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div>
+      <input type={"text"} placeholder="Hours"></input>
+      <input type={"text"} placeholder="Minutes"></input>
+    </div>
+  );
+}
+
+export default Form;
